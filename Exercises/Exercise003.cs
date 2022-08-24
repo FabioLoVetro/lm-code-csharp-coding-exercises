783b8e5d0e2a604331ae7828050a2ad2621bc72c
Fabio <fabio.lovetro@gmail.com>
2022-08-24 09:31:33 +0100

    Exercise003

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -7,16 +7,18 @@ namespace Exercises
         public string[] IceCreamFlavours = 
             { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
-        // position of the flavour
+        // position of the flavour. if the flavour is not present, return -1
         public int IceCreamCode(string iceCreamFlavour)
         {
+            int code = -1;
             for (int i = 0; i < IceCreamFlavours.Length; i++)
             {
                 if (IceCreamFlavours[i] == iceCreamFlavour)
                 {
-                    return i;
+                    code = i;
                 }
             }
+            return code;
         }
     }
 }
