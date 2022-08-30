30bd0b8e5acd5e41db3dcfee8cbd9e900c8049d5
Fabio <fabio.lovetro@gmail.com>
2022-08-30 13:03:25 +0100

    refactoring following c# style

--- Exercises/Models/Person.cs
+++ Exercises/Models/Person.cs
@@ -2,15 +2,6 @@
 
 public class Person
 {
-    ///<value>Property <c>firstName</c> the name of the person.</value>
-    private string firstName = "";
-    ///<value>Property <c>lastName</c> the surname of the person.</value>
-    private string lastName = "";
-    ///<value>Property <c>city</c> the city of the person.</value>
-    private string city = "";
-    ///<value>Property <c>age</c> the age of the person.</value>
-    private int age = 0;
-
     /// <summary>
     /// Constructor <c>Person</c>
     /// <param name = firstName> the name of the person. </param>
@@ -26,75 +17,28 @@ public class Person
         this.age = age;
     }
     /// <summary>
-    /// Method <c>getFirstName</c>
-    /// getter for firstName.
+    /// Method <c>firstName</c>
+    /// getter and setter for firstName.
     /// <return> a string the name of the person. </return>
     /// </summary>
-    public string getFirstName()
-    {
-        return this.firstName;
-    }
+    public string firstName { get; set; }
     /// <summary>
-    /// Method <c>getLastName</c>
-    /// getter for lastName.
+    /// Method <c>lastName</c>
+    /// getter and setter for lastName.
     /// <return> a string the surname of the person. </return>
     /// </summary>
-    public string getLastName()
-    {
-        return this.lastName;
-    }
+    public string lastName { get; set; }
     /// <summary>
-    /// Method <c>getCity</c>
-    /// getter for city.
+    /// Method <c>city</c>
+    /// getter and setter for city.
     /// <return> a string the city of the person. </return>
     /// </summary>
-    public string getCity()
-    {
-        return this.city;
-    }
+    public string city { get; set; }
     /// <summary>
-    /// Method <c>getAge</c>
-    /// getter for age.
+    /// Method <c>age</c>
+    /// getter and setter for age.
     /// <return> an in the age of the person. </return>
     /// </summary>
-    public int getAge()
-    {
-        return this.age;
-    }
-    /// <summary>
-    /// Method <c>setFirstName</c>
-    /// setter for firstName.
-    /// <param name = firstName> the name of the person. </param>
-    /// </summary>
-    public void setFirstName(string firstName)
-    {
-        this.firstName = firstName;
-    }
-    /// <summary>
-    /// Method <c>setLastName</c>
-    /// setter for lastName.
-    /// <param name = lastName> the surname of the person. </param>
-    /// </summary>
-    public void setLastName(string lastName)
-    {
-        this.lastName = lastName;
-    }
-    /// <summary>
-    /// Method <c>setCity</c>
-    /// setter for city.
-    /// <param name = city> the city of the person. </param>
-    /// </summary>
-    public void setCity(string city)
-    {
-        this.city = city;
-    }
-    /// <summary>
-    /// Method <c>setAge</c>
-    /// setter for setAge.
-    /// <param name = age> the age of the person. </param>
-    /// </summary>
-    public void setAge(int age)
-    {
-        this.age = age;
-    }
+    public int age { get; set; }
+  
 }
